fd795fcd60709dea85331bd4e1a24b10ff70b9d8
andygoo <i@jiesc.net>
2016-09-03 10:47:51 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- views/ad_list.php
+++ views/ad_list.php
@@ -38,11 +38,11 @@
 <table class="table table-hover table-bordered">
 <thead>
 <tr>
-	<th>位置</th>
 	<th>标题</th>
-	<th>时间</th>
+	<th>限定时间</th>
 	<th>平台</th>
 	<th>城市</th>
+	<th>最近修改</th>
 	<th width="150">操作</th>
 </tr>
 </thead>
@@ -62,7 +62,6 @@ if (!empty($item['city'])) {
 }
 ?>
 <tr>
-	<td><?= isset($types[$item['type']]) ? $types[$item['type']] : '' ?></td>
 	<td>
 	<?php $link_url = !empty($item['link_url']) ? $item['link_url'] : '#'?>
 	<a href="<?= $link_url?>" target="_blank">
@@ -70,13 +69,14 @@ if (!empty($item['city'])) {
 	        <?= $item['title'] ?><br>
 	    <?php endif;?>
 	    <?php if(!empty($item['pic_url'])):?>
-	        <img width="100" src="<?= $item['pic_url'] ?>">
+	        <img src="<?= $item['pic_url'] ?>" style="max-width: 320px;max-height: 170px;">
 	    <?php endif;?>
 	</a>
 	</td>
-	<td><?= date('Y-m-d H:i:s', $item['updated_at']) ?></td>
+	<td><?= !empty($item['end_time']) ? date('Y-m-d H:i', $item['start_time']) . '<br>至<br>' . date('Y-m-d H:i', $item['end_time']) : '无' ?></td>
 	<td><?= implode(', ', $plat_arr) ?></td>
 	<td><?= $city_str ?></td>
+	<td><?= date('Y-m-d H:i', $item['updated_at']) ?></td>
 	<td>
 	    <a href="<?= URL::site('ad/edit?id='.$item['id']);?>" class="btn btn-info btn-xs ajax-click">修改</a>&nbsp;&nbsp;&nbsp;&nbsp;
 	    <a href="<?= URL::site('ad/del?id='.$item['id']);?>" class="btn btn-info btn-xs ajax-update">删除</a>
