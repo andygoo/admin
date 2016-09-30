d7e1c8c139d0e15d8c889ba76f0b2d12f683aa22
andygoo <i@jiesc.net>
2016-09-30 11:28:21 +0800

    Signed-off-by: andygoo <i@jiesc.net>

--- classes/Controller/Qiniu.php
+++ classes/Controller/Qiniu.php
@@ -7,16 +7,25 @@ use Qiniu\Storage\BucketManager;
 
 class Controller_Qiniu extends Controller_Website {
 
-    public function action_uptoken_hc51() {
-        $qiniu_config = Kohana::config('qiniu.haoche51');
-        $accessKey = $qiniu_config['access_key'];
-        $secretKey = $qiniu_config['secret_key'];
-    
-        $bucket = 'haoche51post';
-        $auth = new Auth($accessKey, $secretKey);
-        $token = $auth->uploadToken($bucket);
-        echo json_encode(array('uptoken'=>$token));
-        exit;
+    public function action_add() {
+        $img_url = Arr::get($_GET, 'img_url');
+        $file_size = Arr::get($_GET, 'file_size');
+        $img_width = Arr::get($_GET, 'img_width');
+        $img_height = Arr::get($_GET, 'img_height');
+        if (!empty($img_url)) {
+            $file_type = explode('.', $img_url);
+            $data = array(
+                    'user_name' => $this->user['username'],
+                    'file_src' => $img_url,
+                    'file_type' => end($file_type),
+                    'file_size' => $file_size,
+                    'img_width' => $img_width,
+                    'img_height' => $img_height,
+                    'add_time' => strtotime('now'),
+            );
+            $uploadModel = Model::factory('upload_qiniu');
+            $uploadModel->insert($data);
+        }
     }
     
     public function action_uptoken() {
@@ -25,6 +34,18 @@ class Controller_Qiniu extends Controller_Website {
         $secretKey = $qiniu_config['secret_key'];
     
         $bucket = 'jiesc-net';
+        $auth = new Auth($accessKey, $secretKey);
+        $token = $auth->uploadToken($bucket);
+        echo json_encode(array('uptoken'=>$token));
+        exit;
+    }
+    
+    public function action_uptoken_hc51() {
+        $qiniu_config = Kohana::config('qiniu.haoche51');
+        $accessKey = $qiniu_config['access_key'];
+        $secretKey = $qiniu_config['secret_key'];
+    
+        $bucket = 'haoche51post';
         $auth = new Auth($accessKey, $secretKey);
         $token = $auth->uploadToken($bucket);
         echo json_encode(array('uptoken'=>$token));
@@ -57,43 +78,61 @@ class Controller_Qiniu extends Controller_Website {
         $this->content->pager = $pager;
     }
     
-    public function action_add() {
-        $img_url = Arr::get($_GET, 'img_url');
-        $file_size = Arr::get($_GET, 'file_size');
-        $img_width = Arr::get($_GET, 'img_width');
-        $img_height = Arr::get($_GET, 'img_height');
-        if (!empty($img_url)) {
-            $file_type = explode('.', $img_url);
-            $data = array(
-                'user_name' => $this->user['username'],
-                'file_src' => $img_url,
-                'file_type' => end($file_type),
-                'file_size' => $file_size,
-                'img_width' => $img_width,
-                'img_height' => $img_height,
-                'add_time' => strtotime('now'),
-            );
-            $uploadModel = Model::factory('upload_qiniu');
-            $uploadModel->insert($data);
-        }
-    }
-
     public function action_del() {
         $id = Arr::get($_GET, 'id');
         $uploadModel = Model::factory('upload_qiniu');
-    
-        $ret = $uploadModel->deleteById($id);
-        if ($ret !== false) {
-            $this->redirect(Request::$referrer);
+        $row = $uploadModel->getRowById($id);
+        $url_info = parse_url($row['file_src']);
+        $key = ltrim($url_info['path'], '/');
+        
+        $deleted = $this->_del($key);
+        if ($deleted) {
+            $ret = $uploadModel->deleteById($id);
+            if ($ret !== false) {
+                $this->redirect(Request::$referrer);
+            }
         }
     }
-    
+
+    public function action_del_hc51() {
+        $id = Arr::get($_GET, 'id');
+        $uploadModel = Model::factory('upload_qiniu');
+        $row = $uploadModel->getRowById($id);
+        $url_info = parse_url($row['file_src']);
+        $key = ltrim($url_info['path'], '/');
+        
+        $deleted = $this->_del_hc51($key);
+        if ($deleted) {
+            $ret = $uploadModel->deleteById($id);
+            if ($ret !== false) {
+                $this->redirect(Request::$referrer);
+            }
+        }
+    }
+
     protected function _del($key) {
         $qiniu_config = Kohana::config('qiniu.default');
         $accessKey = $qiniu_config['access_key'];
         $secretKey = $qiniu_config['secret_key'];
     
         $bucket = 'jiesc-net';
+        $auth = new Auth($accessKey, $secretKey);
+    
+        $bucketMgr = new BucketManager($auth);
+        $err = $bucketMgr->delete($bucket, $key);
+        if ($err === null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    protected function _del_hc51($key) {
+        $qiniu_config = Kohana::config('qiniu.haoche51');
+        $accessKey = $qiniu_config['access_key'];
+        $secretKey = $qiniu_config['secret_key'];
+    
+        $bucket = 'haoche51post';
         $auth = new Auth($accessKey, $secretKey);
         
         $bucketMgr = new BucketManager($auth);
